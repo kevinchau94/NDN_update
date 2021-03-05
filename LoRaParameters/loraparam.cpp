b17536c535d467a3a1b007020cb2602c0b5751fb
kevinchau94 <61897574+kevinchau94@users.noreply.github.com>
2021-03-04 23:52:09 -0700

    Update loraparam.cpp

--- LoRaParameters/loraparam.cpp
+++ LoRaParameters/loraparam.cpp
@@ -28,7 +28,7 @@ int main () {
   */
   if (std::cin >> input)
     param_CR = reinterpret_cast<uint8_t>(input.c_str());
-    std::cout << "The value of CR entered was: " << unsigned(param_CR) << std::endl;
+    std::cout << "The value of CR entered was: " << param_CR << std::endl;
   //std::cout << "The value of CR entered was: " << input << std::endl;
   //param_CR = static_cast<uint8_t>(input);
   //std::cout << "The value of CR entered was: " << param_CR << std::endl;
