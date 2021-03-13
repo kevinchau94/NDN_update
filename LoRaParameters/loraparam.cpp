70d3cdf01cdc0ce7949b331b0eb1d18d9ab08943
kevinchau94 <61897574+kevinchau94@users.noreply.github.com>
2021-03-13 14:16:02 -0700

    Update loraparam.cpp

--- LoRaParameters/loraparam.cpp
+++ LoRaParameters/loraparam.cpp
@@ -5,21 +5,28 @@
 #include <sstream>
 
 //uint8_t param_CR;
-uint8_t param_CR;
-uint16_t param_BW;
-uint8_t param_SF;
-uint32_t param_CH;
-int param_test;
+uint8_t* param_CR;
+uint16_t* param_BW;
+uint8_t* param_SF;
+uint32_t* param_CH;
+//int param_test;
+string param_test;
 
 int main () {
   
   //std::string input;
   
-  std::cout << "Please enter LoRa Parameters." << std::endl;
-  
   //ask for and receive user input for CR
   //std::cout << "Please enter the CR value (in format CR_#): ";
-  std::cout << "Please enter the param_test value";
+  
+  std::cin >> param_test;
+  param_CR = new uint8_t[str.length() + 1];
+  memcpy (param_CR, param_test.c_str(), param_test.length() + 1);
+  
+  
+  
+  
+  
   /*
   std::getline(std::cin, input);
   std::stringstream ss (input);
@@ -27,11 +34,12 @@ int main () {
   std::getline(ss, temp);
   uint64_t temp2 = stoul(temp, 0, 0);
   param_CR = static_cast<uint8_t>(temp2);
-  */
+  
   if (std::cin >> param_test){
     //param_CR = reinterpret_cast<const uint8_t>(input.c_str());
     std::cout << "The value of param_test entered was: " << param_test << std::endl;
   }
+  */
   //std::cout << "The value of CR entered was: " << input << std::endl;
   //param_CR = static_cast<uint8_t>(input);
   //std::cout << "The value of CR entered was: " << param_CR << std::endl;
